98e31589a6760a95f02fc2a626e0872ee81e9c3f
Insire <insirethomson@gmail.com>
2015-05-07 16:30:43 +0200

    fix comments YoutubeTest

--- YoutubeTest/Program.cs
+++ YoutubeTest/Program.cs
@@ -31,53 +31,53 @@ namespace YoutubeTest
 
 			Youtube yt = new Youtube(@"D:\Repositories\git Repos\Insirebot\YoutubeTest\client_secret.json");
 
-			//Console.WriteLine("get specific playlists:");
-			//foreach (Uri u in playlists)
-			//	foreach (string key in System.Web.HttpUtility.ParseQueryString(u.Query).AllKeys)
-			//	{
-			//		string id = System.Web.HttpUtility.ParseQueryString(u.Query).Get(key);
+			Console.WriteLine("get specific playlists:");
+			foreach (Uri u in playlists)
+				foreach (string key in System.Web.HttpUtility.ParseQueryString(u.Query).AllKeys)
+				{
+					string id = System.Web.HttpUtility.ParseQueryString(u.Query).Get(key);
 
-			//		foreach (Playlist p in yt.GetPlaylistByID(id))
-			//		{
-			//			Console.WriteLine(p.Snippet.Title);
-			//		}
-			//	}
-			//Console.WriteLine("adding a test playlist");
-			//if (yt.AddPlaylist("test"))
-			//{
-			//	foreach (Playlist x in yt.GetUserPlayLists())
-			//	{
-			//		if (x.Snippet.Title == "test")
-			//		{
-			//			Console.WriteLine("adding a video");
-			//			foreach (Uri u in playlistitems)
-			//			{
-			//				foreach (string key in System.Web.HttpUtility.ParseQueryString(u.Query).AllKeys)
-			//				{
-			//					string id = System.Web.HttpUtility.ParseQueryString(u.Query).Get(key);
+					foreach (Playlist p in yt.GetPlaylistByID(id))
+					{
+						Console.WriteLine(p.Snippet.Title);
+					}
+				}
+			Console.WriteLine("adding a test playlist");
+			if (yt.AddPlaylist("test"))
+			{
+				foreach (Playlist x in yt.GetUserPlayLists())
+				{
+					if (x.Snippet.Title == "test")
+					{
+						Console.WriteLine("adding a video");
+						foreach (Uri u in playlistitems)
+						{
+							foreach (string key in System.Web.HttpUtility.ParseQueryString(u.Query).AllKeys)
+							{
+								string id = System.Web.HttpUtility.ParseQueryString(u.Query).Get(key);
 
-			//					yt.AddSongToPlaylist(id, x.Id);
-			//				}
-			//			}
-			//		}
-			//		if (x.Snippet.Title == "Favorites")
-			//		{
-			//			foreach (PlaylistItem p in yt.GetPlayListItemByPlaylistID(x.Id))
-			//			{
-			//				Console.WriteLine(p.Snippet.Title);
-			//			}
-			//		}
-			//	}
-			//}
+								yt.AddSongToPlaylist(id, x.Id);
+							}
+						}
+					}
+					if (x.Snippet.Title == "Favorites")
+					{
+						foreach (PlaylistItem p in yt.GetPlayListItemByPlaylistID(x.Id))
+						{
+							Console.WriteLine(p.Snippet.Title);
+						}
+					}
+				}
+			}
 
-			//Console.WriteLine("all current playlists:");
-			//foreach (Playlist x in yt.GetUserPlayLists())
-			//{
-			//	Console.WriteLine(x.Snippet.Title);
-			//}
+			Console.WriteLine("all current playlists:");
+			foreach (Playlist x in yt.GetUserPlayLists())
+			{
+				Console.WriteLine(x.Snippet.Title);
+			}
 
-			//Console.WriteLine("remove playlist test");
-			//yt.RemovePlaylistByName("test");
+			Console.WriteLine("remove playlist test");
+			yt.RemovePlaylistByName("test");
 
 			Console.WriteLine("get Playlistitem");
 			foreach (Uri u in restrictedplaylistitems)
